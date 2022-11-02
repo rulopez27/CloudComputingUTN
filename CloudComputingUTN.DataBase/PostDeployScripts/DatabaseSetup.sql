360fbea9a8985d02b0c424cf84b2f3860a7bd79e
rulopez <ruben_guille27@hotmail.com>
2022-11-01 21:22:59 -0700

    Refactored DatabaseSetup.sql

--- CloudComputingUTN.DataBase/PostDeployScripts/DatabaseSetup.sql
+++ CloudComputingUTN.DataBase/PostDeployScripts/DatabaseSetup.sql
@@ -14,25 +14,31 @@ GO
 
 DECLARE @ArtistID INT = 0
 
-INSERT INTO dbo.Artist(ArtistName, ArtistWikiPage) VALUES
-('Vincent Van Gogh', N'https://artsandculture.google.com/project/van-gogh')
+IF NOT EXISTS (SELECT * FROM dbo.Artists WHERE ArtistName = 'Vincent Van Gogh')
+    BEGIN
+        INSERT INTO dbo.Artists(ArtistName, ArtistWikiPage) VALUES
+        ('Vincent Van Gogh', N'https://artsandculture.google.com/project/van-gogh')
 
-SELECT @ArtistID = SCOPE_IDENTITY()
+        SELECT @ArtistID = SCOPE_IDENTITY()
 
-INSERT INTO dbo.Artwork(ArtistId, ArtworkName, ArtworkYear, ArtworkDescription, ArtworkURL) VALUES 
-(@ArtistID, 'The Starry Night', 1889, N'Van Gogh’s night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order.', 
-N'https://artsandculture.google.com/asset/the-starry-night-vincent-van-gogh/bgEuwDxel93-Pg'),
-(@ArtistID, 'Sunflowers', 1889, N'Van Gogh’s paintings of Sunflowers are among his most famous. He did them in Arles, in the south of France, in 1888 and 1889.',
-N'https://artsandculture.google.com/asset/sunflowers-vincent-van-gogh/hwEGmsM-FoHAwA')
-
+        INSERT INTO dbo.Artworks(ArtistId, ArtworkName, ArtworkYear, ArtworkDescription, ArtworkURL) VALUES 
+        (@ArtistID, 'The Starry Night', 1889, N'Van Gogh’s night sky is a field of roiling energy. Below the exploding stars, the village is a place of quiet order.', 
+        N'https://artsandculture.google.com/asset/the-starry-night-vincent-van-gogh/bgEuwDxel93-Pg'),
+        (@ArtistID, 'Sunflowers', 1889, N'Van Gogh’s paintings of Sunflowers are among his most famous. He did them in Arles, in the south of France, in 1888 and 1889.',
+        N'https://artsandculture.google.com/asset/sunflowers-vincent-van-gogh/hwEGmsM-FoHAwA')
+    END
 
 SET @ArtistID = 0
 
-INSERT INTO dbo.Artist(ArtistName, ArtistWikiPage) VALUES
-('Claude Monet', N'https://artsandculture.google.com/project/monetwashere')
+IF NOT EXISTS (SELECT * FROM dbo.Artists WHERE ArtistName = 'Claude Monet')
+    BEGIN
+        INSERT INTO dbo.Artists(ArtistName, ArtistWikiPage) VALUES
+        ('Claude Monet', N'https://artsandculture.google.com/project/monetwashere')
 
-SELECT @ArtistID = SCOPE_IDENTITY()
+        SELECT @ArtistID = SCOPE_IDENTITY()
 
-INSERT INTO dbo.Artwork(ArtistId, ArtworkName, ArtworkYear, ArtworkDescription, ArtworkURL) VALUES 
-(@ArtistID, 'The Thames at Westminster', 1871, N'En otoño de 1870, París estaba asediada durante la guerra franco-prusiana, y Monet huyó de Francia con su joven familia. Se instalaron en Londres, donde pintó esta vista brumosa del Támesis la primavera siguiente.', N'https://artsandculture.google.com/story/VwKyDFXzPLGdIg')
+        INSERT INTO dbo.Artworks(ArtistId, ArtworkName, ArtworkYear, ArtworkDescription, ArtworkURL) VALUES 
+        (@ArtistID, 'The Thames at Westminster', 1871, N'En otoño de 1870, París estaba asediada durante la guerra franco-prusiana, y Monet huyó de Francia con su joven familia. Se instalaron en Londres, donde pintó esta vista brumosa del Támesis la primavera siguiente.', N'https://artsandculture.google.com/story/VwKyDFXzPLGdIg')
+    END
+
 GO
