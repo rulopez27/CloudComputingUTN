bbbf4e0fd88d4051a3ff5e8e68641b918a20c2a9
rulopez <ruben_guille27@hotmail.com>
2022-11-01 20:22:02 -0700

    Added Artwork table

---
+++ CloudComputingUTN.DataBase/dbo/Tables/Artwork.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Artwork]
+(
+	[ArtworkId] INT NOT NULL PRIMARY KEY IDENTITY,
+	ArtistsId INT NOT NULL REFERENCES Artists(ArtistId),
+	ArtworkName VARCHAR(100) NOT NULL,
+	ArtworkDate DATE NULL,
+	ArtworkDescription VARCHAR(300),
+	ArtworkURL VARCHAR(MAX)
+)
