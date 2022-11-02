deda3fb398109fbcfc4916941933a3b2e18fa347
rulopez <ruben_guille27@hotmail.com>
2022-11-01 20:53:40 -0700

    Refactored Artwork.sql

--- CloudComputingUTN.DataBase/dbo/Tables/Artwork.sql
+++ CloudComputingUTN.DataBase/dbo/Tables/Artwork.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Artwork]
 (
 	[ArtworkId] INT NOT NULL PRIMARY KEY IDENTITY,
-	ArtistsId INT NOT NULL REFERENCES Artists(ArtistId),
+	ArtistId INT NOT NULL REFERENCES Artists(ArtistId),
 	ArtworkName VARCHAR(100) NOT NULL,
 	ArtworkDate DATE NULL,
 	ArtworkDescription VARCHAR(300),
