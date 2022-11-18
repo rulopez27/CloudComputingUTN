fdfd7a87913d4b744ba5e95babb0c85c53bed22b
rulopez <ruben_guille27@hotmail.com>
2022-11-17 22:15:34 -0700

    Added GetArtists_WhenCalled_ReturnsCollectionOfArtists unit test

--- CloudComputingUTN.Middleware.UnitTests/MuseumDbRepositoryTests.cs
+++ CloudComputingUTN.Middleware.UnitTests/MuseumDbRepositoryTests.cs
@@ -1,16 +1,63 @@
+using CloudComputingUTN.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
 namespace CloudComputingUTN.Middleware.UnitTests
 {
+    [TestFixture]
     public class MuseumDbRepositoryTests
     {
+        DbConnection _connection;
+        DbContextOptions _options;
+        SQLiteDbContext CreateContext() => new SQLiteDbContext(_options);
+        IMuseumDbRepository MuseumDbRepository;
+        public void Dispose() => _connection.Dispose();
+        
         [SetUp]
         public void Setup()
         {
+
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            _options = new DbContextOptionsBuilder<BaseDbContext>().UseSqlite(_connection).Options;
+            
+            using var context = new SQLiteDbContext(_options);
+            if (context.Database.EnsureCreated())
+            {
+                context.AddRange(
+                new Artist
+                {
+                    ArtistName = "Leonardo Da Vinci",
+                    ArtistWikiPage = "https://mockurl.com/DaVinci",
+                    ArtworkGallery = new List<Artwork>()
+                        {
+                            new Artwork{ ArtworkName = "La Gioconda",  ArtworkYear = 1519, ArtworkDescription = "Da Vinci's most famous paint"}
+                        }
+                },
+                new Artist
+                {
+                    ArtistName = "Vincent Van Gogh",
+                    ArtistWikiPage = "https://mockurl.com/VanGogh",
+                    ArtworkGallery = new List<Artwork>()
+                        {
+                            new Artwork{ ArtworkName = "The Starry Night",  ArtworkYear = 1889, ArtworkDescription = "Van Gogh's most famous paint"},
+                            new Artwork{ ArtworkName = "The Sunflowers",  ArtworkYear = 1888, ArtworkDescription = "Van Gogh's first paintings"}
+                        }
+                }
+                );
+                context.SaveChanges();
+            }
+            
+            MuseumDbRepository = new MuseumDbRepository(CreateContext());
         }
 
         [Test]
-        public void Test1()
+        public async Task GetArtists_WhenCalled_ReturnsCollectionOfArtists()
         {
-            Assert.Pass();
+            var artists = await MuseumDbRepository.GetArtists();
+            Assert.IsNotEmpty(artists);
         }
     }
 }
---
+++ CloudComputingUTN.Middleware.UnitTests/SQLiteDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CloudComputingUTN.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudComputingUTN.Middleware.UnitTests
+{
+    internal class SQLiteDbContext : BaseDbContext
+    {
+        public SQLiteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>()
+                .Property(x => x.ArtistId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Artist>()
+                .HasMany(x => x.ArtworkGallery);
+
+            modelBuilder.Entity<Artwork>()
+                .Property(x => x.ArtworkId)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
