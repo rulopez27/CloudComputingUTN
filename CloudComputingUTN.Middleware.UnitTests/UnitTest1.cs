47167b93241b8ed8d34559e63c3b71c9e015d0c8
rulopez <ruben_guille27@hotmail.com>
2022-11-17 21:06:25 -0700

    Created CloudComputingUTN.Middleware.UnitTests

---
+++ CloudComputingUTN.Middleware.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CloudComputingUTN.Middleware.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
