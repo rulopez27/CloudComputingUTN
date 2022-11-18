90cb76e14bcae7afd80a4090331532dfa730eac5
rulopez <ruben_guille27@hotmail.com>
2022-11-17 20:29:19 -0700

    Refactored BaseDbContext.cs

--- CloudComutingUTN.Middleware/BaseDbContext.cs
+++ CloudComutingUTN.Middleware/BaseDbContext.cs
@@ -5,6 +5,7 @@ namespace CloudComputingUTN.Middleware
 {
     public abstract class BaseDbContext : DbContext
     {
+        public BaseDbContext(DbContextOptions options) :base(options) { }
         public virtual DbSet<Artist> Artists { get; set; }
         public virtual DbSet<Artwork> Artworks { get; set; }
     }
