36b486868dd7fa26c233ff3a60c83f04adb38487
rulopez27 <ruben_guille27@hotmail.com>
2023-11-08 00:29:58 -0700

    Refactored ArtistDtoExtension name to ArtistDtoExtensions

---
+++ CloudComputingUTN.WebApp/Extensions/ArtistDtoExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CloudComputingUTN.WebApp.DataAccessLayer;
+
+namespace CloudComputingUTN.WebApp.Extensions
+{
+    public static class ArtistDtoExtensions
+    {
+        public static void CreateArtistLinks(this ArtistDto artistDto, LinkGenerator linkGenerator, IHttpContextAccessor context)
+        {
+            LinkService linkService = new LinkService(linkGenerator, context);
+            artistDto.Links.Add(linkService.Generate("Get", "Artists", new {id = artistDto.ArtistId}, "self", "GET"));
+            artistDto.Links.Add(linkService.Generate("Post", "Artists", null, "create", "POST"));
+            artistDto.Links.Add(linkService.Generate("Put", "Artists", null, "update", "PUT"));
+        }
+    }
+}
