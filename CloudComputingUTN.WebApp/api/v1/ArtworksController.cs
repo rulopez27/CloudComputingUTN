fa391df121d0346c1b7ad53f4f19169f8f79a836
rulopez27 <ruben_guille27@hotmail.com>
2023-11-09 10:45:53 -0700

    Added Get artworks endpoint to Artworks API

--- CloudComputingUTN.WebApp/api/v1/ArtworksController.cs
+++ CloudComputingUTN.WebApp/api/v1/ArtworksController.cs
@@ -1,18 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CloudComputingUTN.Entities;
+using CloudComputingUTN.Middleware;
+using CloudComputingUTN.WebApp.DataAccessLayer;
+using CloudComputingUTN.WebApp.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CloudComputingUTN.WebApp.api.v1
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ArtworksController : ControllerBase
     {
+        private IMuseumDbRepository museumDbRepository;
+        private IMapper _mapper;
+        private IHttpContextAccessor _contextAccessor;
+
+        public ArtworksController(IMuseumDbRepository repository, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            museumDbRepository = repository;
+            _mapper = mapper;
+            _contextAccessor = httpContextAccessor;
+        }
+
+
         // GET: api/<ArtworksController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> Get(LinkGenerator linkGenerator)
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var artworks = await museumDbRepository.GetArtworks();
+                var artworksDtoList = new List<ArtworkDto>();
+                foreach (var artwork in artworks)
+                {
+                    ArtworkDto artworkDto = _mapper.Map<ArtworkDto>(artwork);
+                    artworkDto.CreateArtworkLinks(linkGenerator, _contextAccessor);
+                    artworksDtoList.Add(artworkDto);
+                }
+                return Ok(artworksDtoList);
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.InnerException == null ? ex.Message : ex.InnerException.Message;
+
+                return StatusCode(500, errorMessage);
+            }
         }
 
         // GET api/<ArtworksController>/5
