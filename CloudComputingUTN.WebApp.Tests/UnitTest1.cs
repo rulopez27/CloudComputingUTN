a94218bf119be4f4ad88e194f9db06aa00c5d802
Rubén Guillermo López Guzmán <ruben_guille27@hotmail.com>
2024-03-30 00:02:31 -0700

    Added CloudComputingUTN.WebApp.Tests

---
+++ CloudComputingUTN.WebApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CloudComputingUTN.WebApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
